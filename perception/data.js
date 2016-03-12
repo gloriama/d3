f8b478f5b22aebdd463ad50a68ff13016916d7f9
Gloria Ma <gloria.ma@aya.yale.edu>
2016-03-12 11:01:14 -0800

    Adds new types of data

--- perception/data.js
+++ perception/data.js
@@ -1,4 +1,35 @@
-var data = [
+// People worked with that week, starting with Week 4
+var partners = [
+  ['Stephen', 'Mark S', //1: Recursion/Underbar, Data Structures
+   'Bailey', 'Ian L', 'Nikola', //2: Dance Party, N-Queen, D3
+   'Max C', 'Rebecca', 'Kevin', //3: Chatterbox Client, Backbone, Coffeescript
+   'Alex', 'Hahnbi', 'Joan'], //4: Node, Server-side techniques, Databases
+  ['Suya', 'Lex', 'Kris'], //5: Authentication, Deployment, Angular
+  ['Rachel', 'Max O', 'Greg'], //6: MVP, Greenfield
+  ['Rachel', 'Max O', 'Greg'], //7: Legacy
+  ['Justin', 'Wayne', 'Ahad'], //8: Thesis
+  ['Justin', 'Wayne', 'Ahad'], //9: Thesis
+  ['Justin', 'Wayne', 'Ahad'], //10: Thesis
+  ['Justin', 'Wayne', 'Ahad'], //11: Thesis
+  [] //12: HIR
+];
+
+var queers = [
+  'Kris',
+  'Rebecca',
+  'Nikola',
+  'Wayne'
+];
+
+var tapout = [
+  'Rebecca',
+  'Max C',
+  'Kris',
+  'Benny',
+  'Nikola'
+];
+
+var closeness = [
   ['After...','Week 4','Week 5','Week 6','Week 7','Week 8','Week 9','Week 10','Week 11','Week 12'],
   ['Ahad',2,2,1,1,0,0,0,0,0],
   ['Akash',2,2,1,1,1,0,0,0,1],
@@ -8,7 +39,6 @@ var data = [
   ['Benny',0,0,0,0,0,0,0,1,0],
   ['Egan',2,1,1,1,1,1,1,1,1],
   ['Dakota',0,0,0,0,0,0,0,0,0],
-  ['Gloria',0,0,0,0,0,0,0,0,0],
   ['Greg',2,1,0,0,0,0,0,0,0],
   ['Hahnbi',0,2,1,1,0,1,1,0,0],
   ['Harun',1,1,1,1,0,0,1,0,0],
@@ -35,8 +65,5 @@ var data = [
   ['Spencer',1,2,2,2,2,2,1,2,2],
   ['Stephen',0,0,0,0,0,0,0,1,0],
   ['Suya',0,0,0,0,0,0,0,0,0],
-  ['Wayne',1,1,0,0,0,0,0,0,0],
-  [0,0,0,0,0,0,0,0,0,0],
-  ['Tiffany',0,0,0,0,0,0,0,0,0],
-  ['Steffen',0,0,0,0,0,0,0,0,0,]
+  ['Wayne',1,1,0,0,0,0,0,0,0]
 ];
