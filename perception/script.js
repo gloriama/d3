b6e461dc1a7bbea7f8582c079dc8110cb0c827a1
Gloria Ma <gloria.ma@aya.yale.edu>
2016-03-12 10:18:08 -0800

    Renders one row of data

--- perception/script.js
+++ perception/script.js
@@ -1,14 +1,40 @@
 // Bind data to DOM elements
-d3.selectAll('div')
-  .data(data)
+// var people = d3.selectAll('div')
+//   .data(data)
+//   .enter()
+//   .append('div')
+//   .style('height', function(d) {
+//     var sumNumbers = d.reduce(function(acc, item) {
+//       if (typeof item !== 'number') {
+//         return acc;
+//       }
+//       return acc + item;
+//     }, 0);
+//     return sumNumbers + 'px';
+//   });
+
+// For each row in data, create a div
+// For each item in row, create a span
+
+d3.select('body').selectAll('div')
+  .data(data[1].slice(1))
   .enter()
   .append('div')
-  .style('height', function(d) {
-    var sumNumbers = d.reduce(function(acc, item) {
-      if (typeof item !== 'number') {
-        return acc;
+  .style({
+    width: function(d) {
+      return '2em';
+    },
+    background: function(d) {
+      switch(d) {
+        case 0:
+          return 'green';
+          break;
+        case 1:
+          return 'yellow';
+          break;
+        case 2:
+          return 'red';
+          break;
       }
-      return acc + item;
-    }, 0);
-    return sumNumbers + 'px';
+    }
   });
