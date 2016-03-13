2a34997eb9a67962502a7946476f228620cfd57d
Gloria Ma <gloria.ma@aya.yale.edu>
2016-03-12 21:02:29 -0800

    Renders person name next to row

--- perception/script.js
+++ perception/script.js
@@ -31,8 +31,6 @@ closenessBy.longestWorkedWith.sort(function(a, b) {
   return daysWorkedWith(b[0]) - daysWorkedWith(a[0]);
 });
 
-console.log(closenessBy.longestWorkedWith.map(function(a) { return a[0]; }));
-
 var numToColor = function(d) {
   switch(d) {
     case 0:
@@ -45,11 +43,13 @@ var numToColor = function(d) {
 };
 
 var updateOrdering = function(ordering) {
-  var people = d3.select('body').selectAll('div.parent-div')
+  var people = d3.select('body').selectAll('div.person-div')
     .data(closenessBy[ordering]);
 
   people[0].forEach(function(person) {
-    d3.select(person).selectAll('div')
+    d3.select(person).selectAll('div.name-div')
+      .text(person.__data__[0]);
+    d3.select(person).selectAll('div.datum-div')
       .data(person.__data__.slice(1))
       .style('background', numToColor);
   });
@@ -60,13 +60,20 @@ var people = d3.select('body').selectAll('div')
   .data(closenessBy['alphabetical'])
   .enter()
   .append('div')
-  .attr('class', 'parent-div');
+  .attr('class', 'person-div');
 
 people[0].forEach(function(person) {
-  d3.select(person).selectAll('div')
+  d3.select(person).selectAll('div.name-div')
+    .data([person.__data__[0]])
+    .enter()
+    .append('div')
+    .attr('class', 'name-div')
+    .text(person.__data__[0]);
+
+  d3.select(person).selectAll('div.datum-div')
     .data(person.__data__.slice(1))
     .enter()
     .append('div')
-    .attr('class', 'child-div')
+    .attr('class', 'datum-div')
     .style('background', numToColor);
 });
