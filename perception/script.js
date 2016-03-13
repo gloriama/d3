3b0d18a03a27ef3f0da727d5cc453aae6ba50c19
Gloria Ma <gloria.ma@aya.yale.edu>
2016-03-12 20:00:44 -0800

    Allows sorting by closeness

--- perception/script.js
+++ perception/script.js
@@ -1,48 +1,74 @@
-// Bind data to DOM elements
-// var people = d3.selectAll('div')
-//   .data(data)
-//   .enter()
-//   .append('div')
-//   .style('height', function(d) {
-//     var sumNumbers = d.reduce(function(acc, item) {
-//       if (typeof item !== 'number') {
-//         return acc;
-//       }
-//       return acc + item;
-//     }, 0);
-//     return sumNumbers + 'px';
-//   });
+var sumNumbers = function(arr) {
+  return arr.reduce(function(acc, item) {
+    return acc + (typeof item === 'number' ? item : 0);
+  }, 0);
+};
 
-// For each row in data, create a div
-// For each item in row, create a span
+var closenessBy = {
+  alphabetical: closeness,
+  closeness: closeness.slice()
+};
+closenessBy.closeness.sort(function(a, b) {
+  return sumNumbers(a) - sumNumbers(b);
+});
 
+var updateOrdering = function(ordering) {
+  var people = d3.select('body').selectAll('div.parent-div')
+    .data(closenessBy[ordering]);
+
+  people[0].forEach(function(person) {
+    d3.select(person).selectAll('div')
+      .data(person.__data__.slice(1))
+      .style({
+        width: function(d) {
+          return '2em';
+        },
+        background: function(d) {
+          switch(d) {
+            case 0:
+              return 'green';
+              break;
+            case 1:
+              return 'yellow';
+              break;
+            case 2:
+              return 'red';
+              break;
+          }
+        }
+      });
+  });
+};
+
+// Run automatically
 var people = d3.select('body').selectAll('div')
-  .data(closeness.slice(1))
+  .data(closenessBy['alphabetical'])
   .enter()
-  .append('div');
+  .append('div')
+  .attr('class', 'parent-div');
 
 people[0].forEach(function(person) {
   d3.select(person).selectAll('div')
-  .data(person.__data__.slice(1))
-  .enter()
-  .append('div')
-  .attr('class', 'inline-div')
-  .style({
-    width: function(d) {
-      return '2em';
-    },
-    background: function(d) {
-      switch(d) {
-        case 0:
-          return 'green';
-          break;
-        case 1:
-          return 'yellow';
-          break;
-        case 2:
-          return 'red';
-          break;
+    .data(person.__data__.slice(1))
+    .enter()
+    .append('div')
+    .attr('class', 'child-div')
+    .style({
+      width: function(d) {
+        return '2em';
+      },
+      background: function(d) {
+        switch(d) {
+          case 0:
+            return 'green';
+            break;
+          case 1:
+            return 'yellow';
+            break;
+          case 2:
+            return 'red';
+            break;
+        }
       }
-    }
-  });
+    });
 });
